015b5acbb7142aaac41387f55d846b480d365b9b
Juan Garzon <jpgarzon@javeriana.edu.co>
2013-09-25 08:59:36 -0500

    otro cambio

--- ejemploguok/src/princgithubok.java
+++ ejemploguok/src/princgithubok.java
@@ -7,6 +7,8 @@ public class princgithubok {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola mundo hhjhj");
+		System.out.println("otro cambio ");
+		//con comentario
 	}
 
 }
