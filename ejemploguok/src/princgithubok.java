80a3bdea7f6cef09d15459c8e9850820ece39fe4
Juan Garzon <jpgarzon@javeriana.edu.co>
2013-09-25 08:10:10 -0500

    juan csu

--- ejemploguok/src/princgithubok.java
+++ ejemploguok/src/princgithubok.java
@@ -6,7 +6,7 @@ public class princgithubok {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hola mundo");
+		System.out.println("hola mundo hhjhj");
 	}
 
 }
